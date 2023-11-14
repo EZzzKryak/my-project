8ed08728d15b60c8d244f68f15040642f238f5c2
Alex <manicynaleksandr@yandex.ru>
2023-11-14 23:12:33 +0300

    fsd structure

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -5,7 +5,9 @@ import { buildPlugins } from "./buildPlugins";
 import { buildResolvers } from "./buildResolvers";
 import { BuildOptions } from "./types/config";
 
-export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
+export function buildWebpackConfig(
+  options: BuildOptions
+): webpack.Configuration {
   const { paths, mode, isDev } = options;
   return {
     mode,
@@ -19,8 +21,8 @@ export function buildWebpackConfig(options: BuildOptions): webpack.Configuration
     module: {
       rules: buildLoaders(options),
     },
-    resolve: buildResolvers(),
-    devtool: isDev ? 'inline-source-map' : undefined,
+    resolve: buildResolvers(options),
+    devtool: isDev ? "inline-source-map" : undefined,
     devServer: isDev ? buildDevServer(options) : undefined,
   };
 }
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,13 +1,14 @@
 import path from "path";
 import webpack from "webpack";
 import { buildWebpackConfig } from "./config/build/buildWebpackConfig";
-import { BuildPaths, buildEnv } from "./config/build/types/config";
+import { buildEnv, BuildPaths } from "./config/build/types/config";
 
 export default (env: buildEnv) => {
   const paths: BuildPaths = {
     entry: path.resolve(__dirname, "src", "index.tsx"),
     build: path.resolve(__dirname, "build"),
     html: path.resolve(__dirname, "public", "index.html"),
+    src: path.resolve(__dirname, "src"),
   };
 
   const mode = env.mode || "development";
