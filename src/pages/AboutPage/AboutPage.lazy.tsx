b82d20b79540db74d7bd2a3e6cbdccb061eaf3c0
Alex <manicynaleksandr@yandex.ru>
2023-11-14 21:18:11 +0300

    initial commit

---
+++ src/pages/AboutPage/AboutPage.lazy.tsx
@@ -0,0 +1,8 @@
+import { lazy } from 'react';
+
+const AboutPageLazy = lazy(() => new Promise((resolve) => {
+    // @ts-ignore
+    setTimeout(() => resolve(import('./AboutPage')), 1000)
+}));
+
+export default AboutPageLazy;
---
+++ src/test.ts
@@ -0,0 +1,4 @@
+export function someFn(arg: number): string {
+    console.log('random string');
+    return 'random string';
+}
