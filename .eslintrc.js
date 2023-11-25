7b75a646d0bc8ddfdf5c3959a8bffa34603e07b2
Alex <manicynaleksandr@yandex.ru>
2023-11-25 15:36:14 +0300

    add jest, add classNames test

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,12 @@ module.exports = {
     env: {
         browser: true,
         es2021: true,
+        jest: true,
     },
     extends: [
         'plugin:react/recommended',
         'airbnb',
-        'plugin:i18next/recommended',
+        // 'plugin:i18next/recommended',
     ],
     parser: '@typescript-eslint/parser',
     parserOptions: {
@@ -19,7 +20,7 @@ module.exports = {
     plugins: [
         'react',
         '@typescript-eslint',
-        'i18next',
+        // 'i18next',
     ],
     rules: {
         'react/jsx-indent': [2, 4],
@@ -37,7 +38,7 @@ module.exports = {
         'import/extensions': 'off',
         'import/no-extraneous-dependencies': 'off',
         'no-underscore-dangle': 'off',
-        'i18next/no-literal-string': ['error', { markupOnly: true }],
+        // 'i18next/no-literal-string': ['error', { markupOnly: true }],
         'linebreak-style': [
             'error',
             'windows',
