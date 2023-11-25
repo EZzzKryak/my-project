a492e83a9b897d99485614b1e7bf52f57a90773b
Alex <manicynaleksandr@yandex.ru>
2023-11-25 15:40:35 +0300

    fix eslint string len errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,7 +26,9 @@ module.exports = {
         'react/jsx-indent': [2, 4],
         'react/jsx-indent-props': [2, 4],
         indent: [2, 4],
-        'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.tsx'] }],
+        'react/jsx-filename-extension': [
+            2,
+            { extensions: ['.js', '.jsx', '.tsx'] }],
         'import/no-unresolved': 'off',
         'import/prefer-default-export': 'off',
         'no-unused-vars': 'warn',
@@ -38,6 +40,7 @@ module.exports = {
         'import/extensions': 'off',
         'import/no-extraneous-dependencies': 'off',
         'no-underscore-dangle': 'off',
+        'max-len': ['error', { ignoreComments: true }],
         // 'i18next/no-literal-string': ['error', { markupOnly: true }],
         'linebreak-style': [
             'error',
