997450a488c8f25207ba0253972bb3a9f802e098
Alex <manicynaleksandr@yandex.ru>
2023-12-07 20:53:18 +0300

    upgrade sidebar, add button themes

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,6 +1,4 @@
-import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib/classNames/classNames';
-import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -8,22 +6,7 @@ interface NavbarProps {
 }
 
 export function Navbar({ className }: NavbarProps) {
-    const { t } = useTranslation();
     return (
-        <div className={classNames(cls.navbar, {}, [className])}>
-            <div className={cls.links}>
-                <AppLink
-                    theme={AppLinkTheme.SECONDARY}
-                    className={cls.mainLink}
-                    // eslint-disable-next-line i18next/no-literal-string
-                    to="/about"
-                >
-                    {t('О сайте')}
-                </AppLink>
-                <AppLink theme={AppLinkTheme.SECONDARY} to="/">
-                    {t('Главная')}
-                </AppLink>
-            </div>
-        </div>
+        <div className={classNames(cls.navbar, {}, [className])} />
     );
 }
